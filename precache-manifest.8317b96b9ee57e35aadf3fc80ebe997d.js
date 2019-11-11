4c7444a3c27158f625512d1cd07f3d52b383f5ce
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-11 16:03:00 -0300

    Updates

---
+++ precache-manifest.8317b96b9ee57e35aadf3fc80ebe997d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f2097b3b386d6eddf7e5636766129d",
+    "url": "/myport/index.html"
+  },
+  {
+    "revision": "38c610b9156817e7c470",
+    "url": "/myport/static/js/2.cef49e75.chunk.js"
+  },
+  {
+    "revision": "82160c708e5187b087f7",
+    "url": "/myport/static/js/main.6bc0afae.chunk.js"
+  },
+  {
+    "revision": "87215d3f10eb87d2304b",
+    "url": "/myport/static/js/runtime-main.c155b293.js"
+  },
+  {
+    "revision": "c46e288148cc3bac56b8df1598fc8bac",
+    "url": "/myport/static/media/background.c46e2881.png"
+  },
+  {
+    "revision": "55ccd19b16919ddc7fe0874a3c3bd1fa",
+    "url": "/myport/static/media/profile.55ccd19b.jpg"
+  }
+]);
