7c694e9fdb06e9bc4a737503ca561a549d9fb96d
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-11 16:45:39 -0300

    create .env file

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import Home from '~/pages/Home';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
