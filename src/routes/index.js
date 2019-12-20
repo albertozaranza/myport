e5daa1ca6c2a30ad5288aab78766c8e95d481777
Alberto Zaranza <albertozaranza@outlook.com>
2019-12-20 02:06:10 -0300

    test changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from '~/pages/Home';
 
 export default function Routes() {
   return (
-    <BrowserRouter basename="/myport">
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
   );
 }
